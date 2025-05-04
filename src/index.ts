3699370111b0df22e2377a2786ac8a0f46feadfe
Dmitriy Zverev <opensource@dmitriyzverev.ru>
2025-05-05 00:17:59 +0700

    MINOR: Integrate API analyzer (api-extractor) into the project

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-export {type RunCli, type RunCliArgs, type Process, runCli} from './cli/runCli.js';
+export {
+    runCli,
+    type RunCli,
+    type RunCliArgs,
+    type Process,
+    type RootArgs,
+    type NamedCommandDescriptor,
+    type BuildCommand,
+    type HandlerServices,
+    type CommandDescriptor,
+    type RootCommands,
+} from './cli/runCli.js';
 export {defaultLog, type Log, type LogType} from './cli/log.js';
